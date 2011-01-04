8a14f1c75af5aa93dcdcaff3856b730508ff06c2
Kevin Hughes <kevtufc@gmail.com>
2011-01-04 13:07:35 +0000

    Bare rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilp2::Application.config.secret_token = 'f7a20d7674aaccb7f40df79b45f0856bb7b97934f09122d8b8a169c129b8491faf84f236ea6d8970288c6b08f148adc35a1ee54bbdbf919e654e69bfa84a679f'
