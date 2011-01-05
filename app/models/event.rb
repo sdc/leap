87a5aebfd9ff769835b62e640428c11f3474161c
Kevin Hughes <kevtufc@gmail.com>
2011-01-05 13:45:12 +0000

    Simple note model with automatic creation of an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ActiveRecord::Base
+
+  validates :person_id,      :presence => true
+  validates :event_date,     :presence => true
+  validates :eventable_id,   :presence => true
+  validates :eventable_type, :presence => true
+
+  belongs_to :eventable, :polymorphic => true
+
+  before_validation {|event| update_attribute("person_id", event.eventable.person_id)}
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,7 @@
 class Note < ActiveRecord::Base
+
+  has_many :events, :as => :eventable
+
+  after_create {|note| note.events.create(:event_date => created_at)}
+
 end
