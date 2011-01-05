4aaf631a195b2983626191f9450a90b0c444f7bd
Kevin Hughes <kevtufc@gmail.com>
2011-01-05 14:32:01 +0000

    Tests for Note and Event creation

--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "Create Note with Event" do
+    new_note = Note.create(:person_id => 1, :body => "Some body text!")
+    assert(new_note.events)
+    assert_equal(new_note.events.size,1)
+    new_event = new_note.events.first
+    assert_equal(new_event.event_date,new_note.created_at)
+    assert_equal(new_event.person_id,new_note.person_id)
   end
+
 end
