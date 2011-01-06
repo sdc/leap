dc660755d7446a1614170e4823233b683c74e50f
Kevin Hughes <kevtufc@gmail.com>
2011-01-06 09:04:03 +0000

    Small changes to look and some HTML5 stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.all.map{|event| event.eventable}
+    @events = Event.all(:include => :eventable).map{|event| event.eventable}
   end
 
 end
