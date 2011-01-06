b6ccde90bd09a79c7c90264e906135388621163e
Kevin Hughes <kevtufc@gmail.com>
2011-01-06 13:37:00 +0000

    Make date select for event list work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,12 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.includes(:eventable).order("event_date DESC")#.map{|event| event.eventable}
+    @date = (if params[:date]
+      Date.civil(params[:date][:year].to_i,params[:date][:month].to_i,params[:date][:day].to_i)
+    else
+      Date.today
+    end) + 1
+    @events = Event.where("event_date < ?", @date).includes(:eventable).order("event_date DESC")
   end
 
 end
