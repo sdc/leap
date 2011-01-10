bc1029abc6dd2eef2df7ee71e59b622f23e1b6f0
Kevin Hughes <kevtufc@gmail.com>
2011-01-10 16:27:17 +0000

    Ajax inserting of new events on the top

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,13 @@ class EventsController < ApplicationController
 
   def index
     @date = (if params[:date]
-      Date.civil(params[:date][:year].to_i,params[:date][:month].to_i,params[:date][:day].to_i)
+      Time.gm(params[:date][:year].to_i,params[:date][:month].to_i,params[:date][:day].to_i)
     else
-      Date.today
-    end) + 1
-    @events = Event.where("event_date < ?", @date).includes(:eventable).order("event_date DESC")
+      Time.now.midnight
+    end) + 1.day
+    @since = (Time.parse(params[:since]) + 1) if params[:since]
+    @events = Event.where("event_date < ? and event_date > ?", @date,@since||@date - 2.week).includes(:eventable).order("event_date DESC")
+    render :text => "" if @events.first.nil?
   end
 
 end
