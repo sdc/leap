a52424d2679aef41717b7733681f249e34e681d5
Kevin Hughes <kevtufc@gmail.com>
2011-01-11 11:31:06 +0000

    Little bit of refactoring

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,22 @@
 class EventsController < ApplicationController
 
   def index
-    @date = (if params[:date]
-      Time.gm(params[:date][:year].to_i,params[:date][:month].to_i,params[:date][:day].to_i)
-    else
-      Time.now.midnight
-    end) + 1.day
+    @date = get_date + 1.day
     @since = (Time.parse(params[:since]) + 1) if params[:since]
     @events = Event.where("event_date < ? and event_date > ?", @date,@since||@date - 2.week).includes(:eventable).order("event_date DESC")
     render :text => "" if @events.first.nil?
   end
+
+  private
+
+  def get_date
+    if params[:date]
+      Time.gm(params[:date][:year].to_i,params[:date][:month].to_i,params[:date][:day].to_i)
+    else
+      Time.now.midnight
+    end
+  end
+ 
+
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,22 +5,21 @@ class Event < ActiveRecord::Base
   validates :eventable_id,   :presence => true
   validates :eventable_type, :presence => true
 
-  [:title,:subtitle,:icon_url,:body].each do |method|
-    define_method method do
-      if eventable.respond_to?(method) 
-        if eventable.method(method).arity == 1
-          eventable.send(method,event_date)
-        else
-          eventable.send(method)
-        end
-      else
-        nil
-      end
-    end
-  end
-
   belongs_to :eventable, :polymorphic => true
 
   before_validation {|event| update_attribute("person_id", event.eventable.person_id)}
+
+  [:title,:subtitle,:icon_url,:body].each do |method|
+    define_method method do
+      if eventable.respond_to?(method) 
+        m = eventable.method(method)
+        if m.arity == 1
+          m.call(event_date)
+        else
+          m.call
+        end
+      end
+    end
+  end
 
 end
