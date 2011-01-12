edeea57c6d005da628c33378b9fca0e2b7a53dbd
Kevin Hughes <kevtufc@gmail.com>
2011-01-12 11:19:28 +0000

    Navigation side panel (non-functional at the moment).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def icon_link(text,link="#",icon=false)
+      content_tag(:div, :class => "icon_button") do 
+        content_tag(:div, :class => "icon") do
+          link_to link do
+            image_tag("icons/#{icon or text.downcase}.png", :size => "30x30")
+          end
+        end +
+        content_tag(:div, :class => "label")  do
+          link_to text, link
+        end
+      end
+  end
+
 end
