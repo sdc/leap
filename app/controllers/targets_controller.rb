4772b291fbbd3e4cc5372b022ce510a6d709ffa1
Kevin Hughes <kevtufc@gmail.com>
2011-01-13 09:06:54 +0000

    Make target setting work

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -4,4 +4,9 @@ class TargetsController < ApplicationController
     @event = Event.find(params[:event_id])
   end
 
+  def create
+    @event = Event.find(params[:target][:event_id])
+    @target = @event.targets.create(params[:target].merge(:person_id => @event.person_id))
+  end
+
 end
