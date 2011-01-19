d1c64d8741f347537507c6d2d29a13e4c31c47cc
Kevin Hughes <kevtufc@gmail.com>
2011-01-19 16:43:08 +0000

    Toggle hide/show extended buttons on events

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,19 @@
 document.observe("dom:loaded", function(){
   $$('.extend_button').each(function(element){
     element.observe('ajax:complete',function(event){
-      event.findElement('article').down('.extended .inner').update(event.memo.responseText);
-      Effect.SlideDown(event.findElement('article').down('.extended'));
+      article = event.findElement('article')
+      article.down('.extended .inner').update(event.memo.responseText);
+      Effect.SlideDown(article.down('.extended'));
+      article.down('.close_extend_button').show();
+      article.down('.extend_button').hide();
+    })
+  })
+  $$('.close_extend_button').each(function(element){
+    element.observe('click',function(event){
+      article = event.findElement('article');
+      Effect.SlideUp(article.down('.extended'));
+      article.down('.extend_button').show();
+      article.down('.close_extend_button').hide();
     })
   })
 })
