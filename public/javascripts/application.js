fb21979a5001b5e8495363812e566f61712f7b4b
Kevin Hughes <kevtufc@gmail.com>
2011-01-20 13:49:17 +0000

    Fix some real issues with js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,20 @@
 function open_extended_event(ev){
   ev = $(ev);
-  Effect.SlideDown(ev.down('.extended'));
+  Effect.SlideDown(ev.down('.extended'), {queue: 'end'});
   ev.down('.close_extend_button').show();
   ev.down('.extend_button').hide();
 }
 
 function close_extended_event(ev){
   ev = $(ev);
-  Effect.SlideUp(ev.down('.extended'));
+  Effect.SlideUp(ev.down('.extended'), {queue: 'end'});
   ev.down('.extend_button').show();
   ev.down('.close_extend_button').hide();
 }
 
-function watch_events(){  
+function watch_events(e){  
   // Buttons for opening/closing extended sections of events
-  $$('.extend_button').each(function(element){
+  $(e).select('.extend_button').each(function(element){
     element.observe('ajax:complete',function(event){
       article = event.findElement('article')
       article.down('.extended .inner').update(event.memo.responseText);
@@ -24,7 +24,7 @@ function watch_events(){
       open_extended_event(article);
     })
   })
-  $$('.close_extend_button').each(function(element){
+  $(e).select('.close_extend_button').each(function(element){
     element.observe('click',function(event){
       article = event.findElement('article');
       close_extended_event(article);
@@ -36,12 +36,13 @@ function watch_events(){
 function init_new_target_form(element){
   $(element).observe('ajax:complete', function(event){
     close_extended_event(event.findElement('article'));   
+    the_id = article.identify();
     event.findElement('article').replace(event.memo.responseText);
-    watch_events();
+    watch_events(the_id);
   })
 }
 
 document.observe("dom:loaded", function(){
-    watch_events();
+    watch_events("events");
 })
 
