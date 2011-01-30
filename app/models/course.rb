4747c285e4b13f4b653218a408bcda70d5069b9c
Kevin Hughes <kevtufc@gmail.com>
2011-01-30 17:05:39 +0000

    Events for apps and enrolments

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,10 @@ class Course < ActiveRecord::Base
   has_many :person_courses
   has_many :people, :through => :person_courses
 
+  def name(options = {})
+    title + " (#{code})"
+  end
+
   def Course.get(mis_id,fresh=false)
     (fresh ? false : find_by_mis_id(mis_id)) or MisConnector.get_course(mis_id)
   end
--- app/models/person_course.rb
+++ app/models/person_course.rb
@@ -4,4 +4,28 @@ class PersonCourse < ActiveRecord::Base
   belongs_to :course, :dependent => :destroy
   has_many :events, :as => :eventable, :dependent => :destroy
 
+  after_save do |person_course|
+    ["application_date","enrolment_date","end_date"].each do |attr|
+      if person_course.send("#{attr}_changed?") and person_course.send("#{attr}_was") == nil
+        person_course.events.create!(:event_date => person_course.send(attr))
+      end
+    end
+  end
+
+  def icon_url
+    "events/course.png"
+  end
+
+  def title(date)
+    case date
+      when application_date then "Application"
+      when enrolment_date then "Enrolment"
+      when end_date then "Completion" # TODO: distinguish between complete, w/d, incomplete etc
+    end
+  end
+
+  def body
+    course.name
+  end
+
 end
