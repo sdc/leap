db00610010feae0031098d93c47d5ba32a4f85af
Kevin Hughes <kevtufc@gmail.com>
2011-03-29 13:46:58 +0100

    Some database constaints

--- db/migrate/20110105125535_create_events.rb
+++ db/migrate/20110105125535_create_events.rb
@@ -1,14 +1,15 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
-      t.integer :person_id
-      t.string :eventable_type
-      t.integer :eventable_id
-      t.datetime :event_date
-      t.integer :parent_id
-      t.string :transition
+      t.integer  :person_id
+      t.string   :eventable_type, :null => false
+      t.integer  :eventable_id,   :null => false
+      t.datetime :event_date,     :null => false
+      t.integer  :parent_id
+      t.string   :transition
       t.timestamps
     end
+    add_index(:events, :person_id,:eventable_type,:eventable_id,:event_date,:parent_id,:transition)
   end
 
   def self.down
