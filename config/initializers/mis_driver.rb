4aa67e1e4827f012345f5d264f638e7e4da010ea
Kevin Hughes <kevtufc@gmail.com>
2011-03-29 13:47:30 +0100

    Go back to seeing ebs as default

--- config/initializers/mis_driver.rb
+++ config/initializers/mis_driver.rb
@@ -1,4 +1,4 @@
-require 'mis_connectors/no_connector'
+require 'mis_connectors/ebs_connector'
 Ilp2::Application.config.mis_photo_path = "/media/photos"
 Ilp2::Application.config.mis_progress_codes = {"CA"   => :complete,
                                                "CNA"  => :incomplete,
