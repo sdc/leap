8a3b3a94a72ae8c135aba5aa9d85bb13ceb5b47c
Kevin Hughes <kevtufc@gmail.com>
2011-06-08 13:31:42 +0100

    More work on event views. Some of this is getting a bit complex. Consider dropping blueprint columns *within* the events (although not outside)

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ActiveRecord::Base
 
   has_many :events, :as => :eventable, :dependent => :destroy
   belongs_to :person
-  #scope order(:week_beginning)
+  default_scope :order => 'week_beginning'
 
   after_create do |attendance|
     attendance.events.create!(:event_date => week_beginning, :transition => :complete)
@@ -25,7 +25,11 @@ class Attendance < ActiveRecord::Base
   end
 
   def body
-    "Attendance this week: #{att_week}%, overall: #{att_year}"
+    "<img src='https://chart.googleapis.com/chart?" +
+    "chs=200x48&cht=ls&chco=0077CC&chf=bg,s,00000000&chd=t:" +
+    person.attendances.select{|a| a.week_beginning < week_beginning}.map{|a| a.att_year}.join(',') +
+    "' />" +
+    "<dl><dt>This week:</dt><dd>#{att_week}%</dd><dt>Last three weeks:</dt><dd>#{att_3_week}%</dd></dl>"
   end
 
 end
