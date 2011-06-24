c2837f7ad0e992fec6cbf2251d845af8f681af15
Brian Crocker <briancrocker81@gmail.com>
2011-06-24 10:54:59 +0100

    removed comments from disciplinary model

--- app/models/disciplinary.rb
+++ app/models/disciplinary.rb
@@ -1,9 +1,4 @@
-# = Note
-#
-# A note is a simple event consisting of a plain text body. It's intended to be used to capture learners' thoughts in order to
-# turn them into SMART targets in a sturctured manner later on.
-#
-# This model is eventable. It creates a single event when it is created.
+
 
 class Disciplinary < ActiveRecord::Base
 
