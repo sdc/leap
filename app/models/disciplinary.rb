1fe62711098eb1c3e1b180c6082681335ad04ae0
Kevin Hughes <kevtufc@gmail.com>
2011-06-27 12:57:21 +0100

    Targets icon

--- app/models/disciplinary.rb
+++ app/models/disciplinary.rb
@@ -4,12 +4,14 @@ class Disciplinary < ActiveRecord::Base
   belongs_to :person
 
   validates :person_id, :presence => true
+  validates :body, :presence => true
+  validates :level, :presence => true, :numericality => true, :inclusion => {:in => 1..3}
 
   after_create {|disciplinary| disciplinary.events.create!(:event_date => created_at)}
 
   # Returns the note eventable icon URL. This is always the same.
   def icon_url
-    "events/disciplinary.png"
+    "events/disciplinaries.png"
   end
 
   # Returns the note eventable  Title. This is always the same.
