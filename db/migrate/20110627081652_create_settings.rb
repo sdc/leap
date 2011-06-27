302d9eb0378998140aa3712bc239f4a8df05c183
Kevin Hughes <kevtufc@gmail.com>
2011-06-27 09:47:07 +0100

    Point the moodle links at the right place

---
+++ db/migrate/20110627081652_create_settings.rb
@@ -0,0 +1,13 @@
+class CreateSettings < ActiveRecord::Migration
+  def self.up
+    create_table :settings, :force => true do |t|
+      t.string :var,   :null => false
+      t.text   :value, :null => false
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :settings
+  end
+end
