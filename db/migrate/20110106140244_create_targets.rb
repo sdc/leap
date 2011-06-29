b81f6bf81d261b6fc4f0fbbda9de77753fe3b572
Kevin Hughes <kevtufc@gmail.com>
2011-06-29 17:09:50 +0100

    Add some db indexes and docs

--- db/migrate/20110106140244_create_targets.rb
+++ db/migrate/20110106140244_create_targets.rb
@@ -11,6 +11,8 @@ class CreateTargets < ActiveRecord::Migration
       t.integer :set_by_person_id
       t.timestamps
     end
+    add_index :targets, :person_id
+    add_index :targets, :event_id
   end
 
   def self.down
