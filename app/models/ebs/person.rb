14c11b1cf06456e824f7e4bd89a5c33c72a92494
Kevin Hughes <kevtufc@gmail.com>
2011-06-29 11:34:42 +0100

    Don't show people with no date of birth

--- app/models/ebs/person.rb
+++ app/models/ebs/person.rb
@@ -2,6 +2,8 @@ class Ebs::Person < Ebs::Model
 
   set_primary_key :person_code
 
+  default_scope where("date_of_birth is not null")
+
   scoped_search :on => [:forename, :surname, :person_code]
 
   has_many :targets
