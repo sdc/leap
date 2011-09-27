28c0b6f916de2681263f69a494fca98e710e78ad
Kevin Hughes <kevtufc@gmail.com>
2011-09-27 11:40:47 +0100

    Refresh old attendances

--- lib/mis_connectors/ebs_connector.rb
+++ lib/mis_connectors/ebs_connector.rb
@@ -127,7 +127,7 @@ module MisPerson
       Date.civil(1900,1,1)
     end
     mis_person.attendances.
-      where("start_week > ?",last_date).
+      #where("start_week > ?",last_date).
       each do |att|
         na=Attendance.find_or_create_by_person_id_and_week_beginning(id,att.start_week)
         na.update_attributes(
