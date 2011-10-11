b520c1e1a6dc1128adf7507749ad5375342a2824
Kevin Hughes <kevtufc@gmail.com>
2011-10-11 15:23:11 +0100

    Mighty refactor of home pages for people and courses. Made everything look a bit nicer (especially the actual code). Still got to sort out how the next lesson block is laid out, I think.

--- app/helpers/attendance_helper.rb
+++ app/helpers/attendance_helper.rb
@@ -18,7 +18,7 @@ module AttendanceHelper
 
   def attendance_graph(attendances)
     "https://chart.googleapis.com/chart?" +
-    "chs=200x48&cht=lc&chco=0077CC&chf=bg,s,00000000&chd=t:" +
+    "chs=260x100&cht=lc&chco=0077CC&chf=bg,s,00000000&chd=t:" +
     attendances.map{|a| a.att_year}.join(',')
   end
 
