e0261b534b6cd173f6f5d91108893117a5ee280e
Kevin Hughes <kevtufc@gmail.com>
2011-10-20 12:51:20 +0100

    Put the fade in person image back

--- app/assets/javascripts/leap.js
+++ app/assets/javascripts/leap.js
@@ -1,21 +1,7 @@
-//document.observe("dom:loaded", function(){
-//  if ($('flash_notice')){
-//    Element.fade.delay(4,'flash_notice');
-//  }
-//  if ($('q')) {
-//    $('q').activate();
-//    if ($('search_extended')) {
-//      $('search_extended').observe("click", function(event){
-//        $('search_form').submit();
-//      })
-//    }
-//  }
   //$('person_photo').down('img').observe("load", function(event){
   //  event.findElement('img').appear();
   //)
-//  $('person_photo').down('img').observe("mouseover", function(event){
-//    event.findElement('img').shake({distance : 2});
-//  })
+//TODO: Port this to jquery when I can see EBS!
 //  $$('.timetable_event').each(function(e){
 //    e.observe('mouseover', function(event){
 //      event.findElement('.timetable_event').addClassName('extended')
@@ -31,4 +17,6 @@ $(document).ready(function(){
     $('#person_photo').effect('shake', { times:2,distance:3}, 100);
   })
   $('#q').focus();
+  $('#search_extended').click(function(){$('#search_form').submit()});
+  $('#person_photo>img').load(function(event){$(event.target).fadeIn()});
 })
