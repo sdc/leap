d2fd815c9cb63e23541e426a210f2f6aaa9ea94a
Kevin Hughes <kevtufc@gmail.com>
2011-10-23 17:07:55 +0100

    named_scope => scope for rails 3.1

--- app/models/review_window.rb
+++ app/models/review_window.rb
@@ -18,6 +18,6 @@ class ReviewWindow < ActiveRecord::Base
 
   has_many :review
 
-  named_scope :live, where("start_date < ? and end_date > ?", Date.today,Date.today)
+  scope :live, where("start_date < ? and end_date > ?", Date.today,Date.today)
 
 end
