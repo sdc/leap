9d7cee01e54f8865b5d54c20cb4843002474dcaf
Kevin Hughes <kevtufc@gmail.com>
2012-03-12 09:54:47 +0000

    Hold batch print reviews for a minute

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -47,8 +47,9 @@ class CoursesController < ApplicationController
   end
 
   def reviews_block
-    @pub =   Review.where(:person_id => @topic.people.map{|p| p.id}, :published => true,  :window => "Review Two").count
-    @unpub = Review.where(:person_id => @topic.people.map{|p| p.id}, :window => "Review Two").count - @pub
+    #@pub =   Review.where(:person_id => @topic.people.map{|p| p.id}, :published => true,  :window => "Review Two").count
+    #@unpub = Review.where(:person_id => @topic.people.map{|p| p.id}, :window => "Review Two").count - @pub
+    render :text => "This is currently under maintainance"
   end
 
   def add
