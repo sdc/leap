b7665a34ec1bbc22cb524c5a54b5cc40eb2eaac6
Kevin Hughes <kevtufc@gmail.com>
2012-07-20 08:26:44 +0100

    New Intervention logs

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -2,6 +2,12 @@ class Intervention < Eventable
 
   after_create {|i| i.events.create!(:event_date => created_at, :transition => :create)}
 
+  after_save do |i|
+    if i.disc_text_changed? and i.disc_text_was.nil?
+      i.events.create!(:event_date => Time.now, :transition => :complete, :parent_id => i.events.creation.first.id)
+    end
+  end
+
   def self.intervention_types
     its = {}
     Settings.intervention_types.split(";").each{|x| b=x.split(':'); its[b.first] = b.last.split(",")}
@@ -10,14 +16,32 @@ class Intervention < Eventable
 
   def icon_url(tr)
     case tr
-    when :create then "events/intervention_referral.png"
+    when :create   then "events/intervention_referral.png"
+    when :complete then "events/disciplinaries.png"
     end
   end
 
   def title(tr)
     case tr
-    when :create then "Intervention Referral"
+    when :create   then "Intervention Referral"
+    when :complete then "Disciplinary"
     end
+  end
+
+  def extra_panes(tr)
+    if tr == :complete
+      {"Referral Details" => "events/tabs/pi_refer_details"}
+    elsif disc_text.nil?
+      {"Disciplinary Outcome" => "events/tabs/disciplinary_outcome"}
+    end
+  end
+
+  def status
+    disc_text ? :incomplete : :current
+  end
+
+  def body
+    disc_text if status == :incomplete
   end
 
 end
