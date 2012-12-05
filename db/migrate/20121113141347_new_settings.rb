a5d47b99eed18ede61d58d530407f299573f23b8
Kevin Hughes <kevtufc@gmail.com>
2012-12-05 20:26:33 +0000

    Fixed the migration to new settings

--- db/migrate/20121113141347_new_settings.rb
+++ db/migrate/20121113141347_new_settings.rb
@@ -1,9 +1,9 @@
 class NewSettings < ActiveRecord::Migration
   def up
-    Settings.admin_users = Settings.admin_users.split(",")
+    Settings.admin_users = Settings.admin_users.split(",").map{|x| Person.find_by_username(x).id}
   end
 
   def down
-    Settings.admin_users = Settings.admin_users.join(",")
+    Settings.admin_users = Settings.admin_users.map{|x| Person.find(x).username}.join(",")
   end
 end
