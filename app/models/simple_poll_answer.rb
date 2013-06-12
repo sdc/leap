7f4490a5a3b11cd944942143b0c53fe52e3f31af
Kevin Hughes <kevtufc@gmail.com>
2013-06-12 22:45:53 +0100

    Validate poll answers so people can't just add their own

--- app/models/simple_poll_answer.rb
+++ app/models/simple_poll_answer.rb
@@ -5,6 +5,9 @@ class SimplePollAnswer < Eventable
   belongs_to :person
   belongs_to :simple_poll
 
+  validates :person_id, :uniqueness => {:scope => :simple_poll_id}
+  validates :answer, :inclusion => {:in => proc { |x| x.simple_poll.answers}}
+
   delegate :question, :results, :to => :simple_poll
 
   after_create {|ans| ans.events.create!(:event_date => created_at, :transition => :create)}
