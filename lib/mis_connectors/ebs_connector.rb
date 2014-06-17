9261dbe14203c60f0a7ea73510e72bcbf9c1c9af
Kevin Hughes <kevtufc@gmail.com>
2014-06-17 15:39:36 +0100

    Take out the Merthyr bodges

--- lib/mis_connectors/ebs_connector.rb
+++ lib/mis_connectors/ebs_connector.rb
@@ -137,11 +137,11 @@ module MisPerson
   def timetable_events(options = {})
     reds = if options == :next
       Ebs::RegisterEventDetailsSlot.where(:object_id => mis_id, :object_type => ['L','T']).
-        where("planned_start_date > ?", Date.today.strftime("%Y-%d-%m %H:%M:%S")).#'2013-20-11 00:00:00'"). 
+        where("planned_start_date > ?",Time.now)
         order(:planned_start_date).limit(1)
     else
-      from = (options[:from] || Date.today.beginning_of_week).strftime("%Y-%d-%m %H:%M:%S")
-      to   = (options[:to  ] || from.end_of_week).strftime("%Y-%d-%m %H:%M:%S")
+      from = (options[:from] || Date.today.beginning_of_week)
+      to   = (options[:to  ] || from.end_of_week)
       Ebs::RegisterEventDetailsSlot.where(:object_id => mis_id, :object_type => ['L','T'], :planned_start_date => from..to)
     end
     reds.map do |s| 
