7a4a5961f32318903aef769483d075fc87747451
Kevin Hughes <kevtufc@gmail.com>
2014-08-22 09:45:11 +0100

    Another year update

--- app/models/entry_req.rb
+++ app/models/entry_req.rb
@@ -8,13 +8,16 @@ class EntryReq < ActiveRecord::Base
   def self.import(file)
     old_logger_level, logger.level = logger.level, Logger::ERROR if logger
     CSV.foreach(file) do |row|
-      if course=Course.find_by_year_and_code("13/14",row[0])
+      if course=Course.find_by_year_and_code("14/15",row[0])
         ["Numeracy","Literacy","General","Specialist","Additional"].each_with_index do |r,i|
           next if row[i+1].blank?
           next if course.entry_reqs.where(:category => r).first
           course.entry_reqs.create(:category => r,:body => row[i+1]) unless row[i+1] == "Please select"
 	  puts "Added #{r} to #{course.name}"
         end
+      else 
+        puts "*" * 40
+        puts row[0] + " - not found!"
       end
     end
   ensure
