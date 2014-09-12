81099dd7daeaa7f02b3c5d18965c60082052d811
Kevin Hughes <kevtufc@gmail.com>
2014-09-12 13:02:08 +0100

    Global News, but no create pages yet

---
+++ app/models/global_news.rb
@@ -0,0 +1,16 @@
+class GlobalNews < ActiveRecord::Base
+  attr_accessible :body, :title
+
+  def to_tile
+    Tile.new({
+      :title => subtitle ? title : "News",
+      :subtitle => subtitle || title,
+      :bg => "4f2170",
+      :icon => "fa-newspaper-o",
+      :modal_title => "#{title} #{subtitle ? "<small>#{subtitle}</small>" : nil}",
+      :modal_body => body,
+      :object => self
+    })
+  end
+ 
+end
---
+++ test/unit/global_news_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GlobalNewsTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
