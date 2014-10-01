1d5500e375d111ae7cb28246f572a535b0e64a1e
Kevin Hughes <kevtufc@gmail.com>
2014-10-01 15:13:41 +0100

    Fix migration for global news

--- db/migrate/20140905124858_create_global_news.rb
+++ db/migrate/20140905124858_create_global_news.rb
@@ -2,6 +2,7 @@ class CreateGlobalNews < ActiveRecord::Migration
   def change
     create_table :global_news do |t|
       t.string :title
+      t.string :subtitle
       t.text :body
       t.timestamps
     end
