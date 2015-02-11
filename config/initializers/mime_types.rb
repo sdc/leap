db8d0f9ddf2abf15bec27d0a38f5b70e2b7d1af1
Kevin Hughes <kevtufc@gmail.com>
2015-02-11 15:09:30 +0000

    This hasn't been needed for a *long* time and keeps spewing an error on startup :)

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,5 +2,4 @@
 
 # Add new mime types for use in respond_to blocks:
 Mime::Type.register "image/jpeg", :jpg
-Mime::Type.register "application/pdf", :pdf, ['text/pdf'], ['pdf']
 # Mime::Type.register_alias "text/html", :iphone
