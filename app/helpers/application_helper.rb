294fb85939cb84985ab63c46d7e66dfb2f4a17b4
Kevin Hughes <kevtufc@gmail.com>
2015-02-12 10:14:55 +0000

    More rubocop fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,6 @@ module ApplicationHelper
   end
 
   def link_to_submit(*args, &block)
-    link_to_function (block_given? ? capture(&block) : args[0]), "$(this).closest('form').submit()", args.extract_options!
+    link_to_function block_given? ? capture(&block) : args[0], "$(this).closest('form').submit()", args.extract_options!
   end
 end
