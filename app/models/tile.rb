edb6e98034806d1442a6f15e4ce0bbf660c22dcb
Kevin Hughes <kevtufc@gmail.com>
2015-02-12 13:11:59 +0000

    Rewrite method as delegation

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,14 +1,11 @@
 class Tile
   include ActiveModel::Conversion
+  delegate :eventable, to: :eventable
 
   attr_accessor :id, :title, :bg, :icon, :is_deletable, :subtitle, :body, :person_id, :partial_path, :object, :link, :modal_title, :modal_body
 
   def initialize(attrs)
     attrs.each { |a| send("#{a.first}=", a.last) if respond_to?("#{a.first}=") }
-  end
-
-  def eventable
-    event.eventable
   end
 
   def partial
