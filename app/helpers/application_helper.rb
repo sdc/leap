2c64e4cbbdfd7056964114885af7aa04e469bbd8
Kevin Hughes <kevtufc@gmail.com>
2015-02-12 16:04:39 +0000

    Fixing some deprecations in rails4.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,4 @@ module ApplicationHelper
     end
   end
 
-  def link_to_submit(*args, &block)
-    link_to_function block_given? ? capture(&block) : args[0], "$(this).closest('form').submit()", args.extract_options!
-  end
 end
