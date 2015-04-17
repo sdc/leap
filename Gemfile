3b2a38874cbad68c080a37564ed906c4d7767cfb
Kevin Hughes <kevtufc@gmail.com>
2015-04-17 09:42:51 +0100

    Bugs picked up from a bootstrap of leap

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
-#gem 'rails', "~> 4.2.0"
-gem 'rails', git: 'https://github.com/rails/rails.git', branch: '4-2-stable'
+gem 'rails', "~> 4.2.0"
+#gem 'rails', git: 'https://github.com/rails/rails.git', branch: '4-2-stable'
 gem 'coffee-rails', "~> 4.1.0"
 gem 'jquery-rails', '~> 4.0.3'
 gem 'jquery-ui-rails', '~> 5.0.0'
--- db/migrate/20121113141347_new_settings.rb
+++ db/migrate/20121113141347_new_settings.rb
@@ -1,7 +1,11 @@
 class NewSettings < ActiveRecord::Migration
   def up
-    if Settings.admin_users.kind_of? String
-      Settings.admin_users = Settings.admin_users.split(",").map{|x| Person.find_by_username(x).id}
+    begin
+      if Settings.admin_users.kind_of? String
+        Settings.admin_users = Settings.admin_users.split(",").map{|x| Person.find_by_username(x).id}
+      end
+    rescue
+      # Yeah. But this only runs on new installs now and it always breaks because the setting API has changed
     end
   end
 
