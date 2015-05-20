2de3fdd69756d336aba015a7811a40de48a62a5e
Kevin Hughes <kevtufc@gmail.com>
2015-05-20 15:33:16 +0100

    Select the right dates for different timeline views

--- app/controllers/timeline_views_controller.rb
+++ app/controllers/timeline_views_controller.rb
@@ -37,7 +37,11 @@ class TimelineViewsController < ApplicationController
       @topic.events
     end
     date = params[:date] ? Date.parse(params[:date]) : Date.today()
-    scope = scope.where(event_date: date.beginning_of_week...date.end_of_week)
+    scope = if view.view_type == "timetable"
+              scope.where(event_date: date.beginning_of_week...date.end_of_week)
+            else
+              scope.where("event_date > ?", date)
+            end
     conds = []
     str = []
     view.events.each do |etype,trans|
