580447798d82e11a907a8db0c5a5723f85600626
Owen Widdicombe <owenwiddicombe@southdevon.ac.uk>
2016-03-03 16:51:10 +0000

    Modernizr js change routes and schedule updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,46 @@
 Ilp2::Application.routes.draw do
+
   namespace :admin do
     resources :settings
+    resources :views
     get 'test'       => 'test#index', :as => :test
-    #get 'stats'      => 'stats#index'
+    #match 'stats'      => 'stats#index'
+    post 'test/login' => 'test#login', :as => :test_login
     get 'sync_grade_tracks' => 'data#sync_grade_tracks'
-    match 'test/login' => 'test#login', :as => :test_login, :via => [:get, :post]
   end
+  resources :views
   resources :timeline_views, only: :index
-  resources :events
+  resources :events do
+    get "more", :on => :collection
+    get "open_extended", :on => :member
+  end
   resources :people do
-    resources :events
+    resources :events, :timetables
     resources :timeline_views, only: [:show,:index]
+    resources :views, :only => [:show] do
+      get "header", :on => :member
+    end
     collection do
       get :search, :select
     end
+    member do
+      get :next_lesson_block, :my_courses_block, :targets_block
+      get :moodle_block, :attendance_block, :poll_block
+    end
   end
   resources :courses do
+    resources :views do
+      get "header", :on => :member
+    end 
+    resources :timetables
+    member do
+      get :next_lesson_block, :moodle_block, :reviews_block, :entry_reqs_block
+      post :add
+    end
     resources :events
     resources :timeline_views
   end
-  root to: "people#index"
+
+
+  root :to => "people#index"
 end
