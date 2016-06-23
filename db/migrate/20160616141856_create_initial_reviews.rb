e87da11660ceea59e98a8c019a37f74d3cf8a6b5
james.byrne <you@example.com>
2016-06-23 11:34:46 +0100

    various changes

--- db/migrate/20160616141856_create_initial_reviews.rb
+++ db/migrate/20160616141856_create_initial_reviews.rb
@@ -1,7 +1,7 @@
 class CreateInitialReviews < ActiveRecord::Migration
   def change
     create_table :initial_reviews do |t|
-      t.integer :progress_id, :limit => 26
+      t.integer :progress_id, :limit => 11
       t.integer :person_id, :limit => 11
       t.string :body, :limit => 500
       t.string :target_grade, :limit => 50
--- db/migrate/20160616142216_create_progress_reviews.rb
+++ db/migrate/20160616142216_create_progress_reviews.rb
@@ -2,7 +2,7 @@ class CreateProgressReviews < ActiveRecord::Migration
   def change
     create_table :progress_reviews do |t|
       t.integer :person_id, :limit => 11
-      t.integer :progress_id, :limit => 26
+      t.integer :progress_id, :limit => 11
       t.integer :number, :limit => 1
       t.string :body, :limit => 500
       t.string :working_at, :limit => 100
