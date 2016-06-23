25ce70c25b8e3f210fa11a16b29b23d2c6c9332a
james.byrne <you@example.com>
2016-06-23 15:11:26 +0100

    finalised notifications

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -51,7 +51,7 @@ class PeopleController < ApplicationController
           ppdc = Settings.moodle_badge_block_courses.try(:split,",")
           @badges = @topic.mdl_badges.where(:mdl_course_id => ppdc) if ppdc && @topic.mdl_badges.where(:mdl_course_id => ppdc).any?
           @aspiration = @topic.aspirations.last.aspiration if @topic.aspirations.present?
-          @notifications = @topic.notifications.where(:notified => false)
+          @notifications = @user.notifications.where(:notified => false)
         else
           @tiles = @topic.events.where(:eventable_type => "Target",:transition => :overdue).
                    where(:event_date => (Date.today - 1.week)..(Date.today + 1.month)).limit(8)
