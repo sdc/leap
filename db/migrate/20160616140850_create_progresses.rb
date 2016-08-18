4a7327ccf5d1d06a178359727e05de842c75c2c1
james.byrne <jamesbyrne@southdevon.ac.uk>
2016-08-18 13:34:59 +0100

    Updated Null on db migrations

--- db/migrate/20160616140850_create_progresses.rb
+++ db/migrate/20160616140850_create_progresses.rb
@@ -1,15 +1,14 @@
 class CreateProgresses < ActiveRecord::Migration
   def change
     create_table :progresses do |t|
-      t.integer :person_id, :limit => 11
-      t.string :uio_id, :limit => 11
-      t.string :course_title, :limit => 255
-      t.string :course_code, :limit => 15
+      t.integer :person_id, :limit => 11, :null => false
+      t.string :uio_id, :limit => 11, :null => false
+      t.string :course_title, :limit => 255, :null => false
+      t.string :course_code, :limit => 15, :null => false
       t.string :course_year, :limit => 5
       t.string :course_type, :limit => 50
       t.string :course_status, :limit => 50
       t.integer :course_tutor_id, :limit => 11
-      t.timestamps
       t.string :bksb_maths_ia, :limit => 100
       t.string :bksb_english_ia, :limit => 100
       t.string :bksb_maths_da, :limit => 100
@@ -17,6 +16,7 @@ class CreateProgresses < ActiveRecord::Migration
       t.float :qca_score
       t.string :nat_target_grade, :limit => 50
       t.string :subject_grade, :limit => 100
+      t.timestamps
     end
   end
 end
--- db/migrate/20160620103005_create_induction_questions.rb
+++ db/migrate/20160620103005_create_induction_questions.rb
@@ -1,9 +1,9 @@
 class CreateInductionQuestions < ActiveRecord::Migration
   def change
     create_table :induction_questions do |t|
-      t.integer :person_id, :limit => 11
-      t.integer :created_by_id, :limit => 11
-      t.string :question, :limit => 255
+      t.integer :person_id, :limit => 11, :null => false
+      t.integer :created_by_id, :limit => 11, :null => false
+      t.string :question, :limit => 255, :null => false
       t.text :answer
       t.date :created_at
 
