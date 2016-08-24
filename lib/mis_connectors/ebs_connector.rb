2688b1b2702f0f6191cfb48520cef881c0f46d2e
owenwiddicombe <owenwiddicombe@southdevon.ac.uk>
2016-08-24 13:34:37 +0100

    Keep out enrichment and Z codes. Filter out invalid quals and invalid grades.  Keep out unit grades.

--- lib/mis_connectors/ebs_connector.rb
+++ lib/mis_connectors/ebs_connector.rb
@@ -253,7 +253,7 @@ module MisPerson
 
   def import_quals
     last_update = qualifications.order("updated_at DESC").first.try(:updated_at) or Date.today - 5.years
-    mis_person.learner_aims.where("uio_id IS NOT NULL and grade IS NOT NULL and updated_date > ?",last_update).each do |la|
+    mis_person.learner_aims.where("uio_id IS NOT NULL and grade IS NOT NULL and updated_date > ? and upper(learning_aim) not like 'Z%%' and upper(learning_aim) != 'ENRICH'",last_update).each do |la|
       next unless la.unit_instance_occurrence 
       next unless Qualification.where(:mis_id => la.id, "import_type" => "la").empty?
       nq=qualifications.create(
@@ -266,7 +266,7 @@ module MisPerson
       )
       nq.update_attribute("mis_id",la.id)
     end
-    mis_person.attainments.each do |at|
+    mis_person.attainments.where("NOT (upper(description) LIKE '%%NOT APPLICABLE%%' and source = 'PLR data') and upper(grade) NOT LIKE 'NA%%' and upper(grade) NOT LIKE 'OTH%%' and upper(grade) NOT LIKE '%%UNCLASSIFIED%%' and upper(grade) NOT LIKE '%%NO SHOW%%' and upper(grade) NOT LIKE '%%P ALPHA-NUMERIC VALUE%%' and upper(grade) != '01' and not regexp_like(trim(fes_qualification_aim), '^[0-9]')").each do |at|
       next unless Qualification.where(:mis_id => at.id, "import_type" => "attainment").empty?
       nq=qualifications.create(
         :title       => at.description,
