5aa5d893d2c980603b28253fd54ad72540c32ff4
Owen Widdicombe <owenwiddicombe@southdevon.ac.uk>
2016-08-25 16:49:57 +0100

    pick up acyr from EBS

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,8 @@
 set :output, "log/cron_log.log"
 
 every 1.day, :at => '10:10pm' do
-  runner "Person.resync('15/16')"
+  # runner "Person.resync('15/16')"
+  runner "Person.resync()"
 end
 
 every 4.hours do
