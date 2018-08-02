b51cb736708f00026cc152af1cd04c60f8fac953
James Kam-Radcliffe <jameskamradcliffe@southdevon.ac.uk>
2018-08-02 12:45:12 +0100

    DB migrations for updated absence table

---
+++ db/migrate/20180802091614_change_absence_column_names.rb
@@ -0,0 +1,9 @@
+class ChangeAbsenceColumnNames < ActiveRecord::Migration
+  def change
+  	change_table :absences do |t|
+  		t.rename :created_by_id, :created_by
+  		t.rename :category, :reason
+  		t.rename :body, :reason_extra
+  		t.rename :contact_category, :contact
+  end
+end
