d86f274092c67e0c58c491d9fa4af829e2eb6d64
James Kam-Radcliffe <jameskamradcliffe@southdevon.ac.uk>
2018-09-14 10:26:42 +0100

    Check for presence of teacher before sending email

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -2,11 +2,13 @@ class TeacherMailer < ActionMailer::Base
   default from: "leap@southdevon.ac.uk"
 
   def email_teacher(teacher, learner, register_event, slot)
-    @teacher = teacher
-    @learner = learner
-    @register_event = register_event
-    @slot = slot
-    mail(to: @teacher.college_email, subject: 'Student Absence')
+  	if teacher.present?
+	    @teacher = teacher
+	    @learner = learner
+	    @register_event = register_event
+	    @slot = slot
+	    mail(to: @teacher.college_email, subject: 'Student Absence')
+	  end
   end 
     
 end
