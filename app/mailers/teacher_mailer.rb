6378abff2e9523102e7c1a2e6ab54755483183ad
James Kam-Radcliffe <jameskamradcliffe@southdevon.ac.uk>
2018-09-27 10:42:23 +0100

    Check for teacher and email before sending absence email

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -2,12 +2,12 @@ class TeacherMailer < ActionMailer::Base
   default from: "leap@southdevon.ac.uk"
 
   def email_teacher(teacher, learner, register_event, slot)
-  	if teacher.present?
+  	if teacher.present? && teacher.college_email.present?
 	    @teacher = teacher
 	    @learner = learner
 	    @register_event = register_event
 	    @slot = slot
-	    mail(to: @teacher.college_email, subject: 'Student Absence')
+	    mail(to: @teacher.college_email, subject: 'Student Absence') if Rails.env != 'development'
 	  end
   end 
     
