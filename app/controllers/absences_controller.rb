3270b48b49a0c63f0bafc2363110f4dc2fee48be
Owen Widdicombe <owenwiddicombe@southdevon.ac.uk>
2019-03-26 11:28:50 +0000

    amalgamate Student Absence emails going to lecturers

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -26,6 +26,9 @@ class AbsencesController < ApplicationController
         end
       end
 
+      learner = Ebs::Person.find(@absence.person_id)
+
+      all_teachers = {}
       slot_ids.each do |si|
         @absence_slot = Ebs::AbsenceSlot.new(absence_id: @absence.id, register_event_details_slot_id: si)
         # @absence_slot.save!
@@ -34,27 +37,27 @@ class AbsencesController < ApplicationController
         slot.usage_code = @absence.usage_code
         # slot.save!
         slot.create_or_update
-        if Rails.env == "development"
-          teachers = []
-          teacher = Ebs::Person.find(30141843)
+
+        teachers = []
+        teacher_slots = Ebs::RegisterEventDetailsSlot.where(planned_start_date: slot.planned_start_date, register_event_id: slot.register_event_id, object_type: 'T')
+        teacher_slots.each do |ts|
+          teacher = Ebs::Person.find(ts.object_id)
           teachers << teacher
-        else
-          teachers = []
-          teacher_slots = Ebs::RegisterEventDetailsSlot.where(planned_start_date: slot.planned_start_date, register_event_id: slot.register_event_id, object_type: 'T')
-          teacher_slots.each do |ts|
-            teacher = Ebs::Person.find(ts.object_id)
-            teachers << teacher
-          end
-          # teacher = Ebs::Person.find(Ebs::RegisterEventDetailsSlot.where(planned_start_date: slot.planned_start_date, register_event_id: slot.register_event_id, object_type: 'T')[0].object_id)
         end
+
         register_event = Ebs::RegisterEvent.find(slot.register_event_id)
-        learner = Ebs::Person.find(slot.object_id)
-        if teachers.count > 0
-          teachers.each do |t|
-            TeacherMailer.email_teacher(t, learner, register_event, slot).deliver
-          end
+        teachers.each do |t|
+          all_teachers[t.person_code] = [].to_set if ! all_teachers.include?(t.person_code)
+          all_teachers[t.person_code] << { :regevent => register_event, :slot => slot }
         end
       end
+
+      all_teachers.each do |t,details|
+        teacher = Ebs::Person.find(t)
+        regevents_and_slots_sorted = details.to_a.sort_by{|ras| [ras[:slot].planned_start_date, ras[:slot].planned_end_date] }
+        TeacherMailer.email_teacher_regevents_and_slots(teacher, learner, regevents_and_slots_sorted).deliver if (regevents_and_slots_sorted.try(:count) || 0) > 0
+      end
+
       redirect_to person_timetables_url person_id: @absence.person_id, date: (params[:absence] && params[:absence][:date] && params[:absence][:date].to_date.is_a?(Date) == true) ? (params[:absence][:date]) : (nil), refresh: true
     end
   end
--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -7,8 +7,23 @@ class TeacherMailer < ActionMailer::Base
 	    @learner = learner
 	    @register_event = register_event
 	    @slot = slot
-	    mail(to: @teacher.college_email, subject: 'Student Absence') if !( Rails.env == 'development' or ( Person.user.superuser? and Settings.su_send_emails != 'on' ) )
+	    mail_to = @teacher.college_email
+	    mail_to = Settings.dev_test_email if Rails.env == 'development' && Settings.dev_send_to_test_email == 'on' && Settings.dev_test_email.present?
+	    # mail(to: @teacher.college_email, subject: 'Student Absence') if !( Rails.env == 'development' or ( Person.user.superuser? and Settings.su_send_emails != 'on' ) )
+	    mail(to: mail_to, subject: 'Student Absence') if !( ( Rails.env == 'development' && Settings.dev_send_to_test_email != 'on' ) or ( Person.user.superuser? and Settings.su_send_emails != 'on' ) )
 	  end
-  end 
-    
+  end
+
+  def email_teacher_regevents_and_slots(teacher, learner, regevents_and_slots)
+    return if (!teacher.present?) || teacher.college_email.empty?
+
+    @teacher = teacher
+    @learner = learner
+    @register_event_slots = regevents_and_slots
+    mail_to = @teacher.college_email
+    mail_to = Settings.dev_test_email if Rails.env == 'development' && Settings.dev_send_to_test_email == 'on' && Settings.dev_test_email.present?
+    mail(to: mail_to, subject: 'Student Absence') if !( ( Rails.env == 'development' && Settings.dev_send_to_test_email != 'on' ) or ( Person.user.superuser? and Settings.su_send_emails != 'on' ) )
+
+  end
+
 end
